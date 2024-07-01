ea4ff9472bff26dad03f56de4c11ed8771d6ad71
Wellington Montagnini <we.montagnini@gmail.com>
2024-06-30 23:43:56 -0300

    REACT ESSENTIALS COMPONENTS - FINISHED.
     - Modulo basico de React finalizado.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,49 @@
+import { useState } from "react";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
+import TabButton from "./components/TabButton.jsx";
+
 function App() {
+  let [selectedTopic, setSelectedTopic] = useState('components')
+  
+
+  function handleClick(selectedButton){
+    setSelectedTopic(selectedButton)
+  }
+
+
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+        <h2>Examples</h2>
+        <menu>
+          <TabButton onClick={() => handleClick('components')}>Components</TabButton>
+          <TabButton onClick={() => handleClick('jsx')}>JSX</TabButton>
+          <TabButton onClick={() => handleClick('props')}>Props</TabButton>
+          <TabButton onClick={() => handleClick('state')}>State</TabButton>
+        </menu>
+        <div id="tab-content">
+    	    <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>
+            {EXAMPLES[selectedTopic].code}
+            </code>
+          </pre>
+        </div>
+        </section>
       </main>
     </div>
   );
